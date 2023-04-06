62de00c3c0ea9de06c012bc933a90b3b342bfc6d
Aleksander Łuczycki <aleksander.luczycki@op.pl>
2023-04-06 18:29:39 +0200

    added image and transform display

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import critical
 import ancillary
+import transform
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
 
 def main():
 
     # Chunks to anonimize
     anon_chunks = [b'dSIG', b'eXIf', b'iTXt', b'tEXt', b'tIME', b'zTXt']
-    # Otwórz plik PNG w trybie binarnym
+    # Open png in byte read mode (also the output file)
     with open('moded.png', 'rb') as file_png, open('out.png', 'wb') as out_file:
         
         # Wczytaj pierwsze 8 bajtów nagłówka
@@ -36,8 +41,8 @@ def main():
                 case b'IEND':
                     critical.read_IEND(file_png, out_file)
                     iend_read = True
+                
                 case _:
-                    print(block_type.decode("utf-8"))
                     if block_type in anon_chunks:
                         ancillary.anon(file_png, out_file, length, block_type.decode("utf-8"))
                     else: continue
@@ -47,8 +52,22 @@ def main():
             out_file.write(crc)
             print("Suma kontrolna {}".format(crc.hex()))
             print(20*"-")
+    
+    img_gray = cv.imread('large.png', cv.IMREAD_GRAYSCALE)
+    img = cv.imread("large.png", cv.IMREAD_UNCHANGED)
 
+    dft = cv.dft(np.float32(img_gray),flags = cv.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+    magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
+    cv.imshow("image", img)
+
+    plt.subplot(111),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+    
+    cv.waitKey(0)
+    
 
 if __name__ == "__main__":
     main()
